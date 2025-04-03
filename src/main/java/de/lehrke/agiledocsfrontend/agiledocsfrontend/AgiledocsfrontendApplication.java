ad0fc19c01d14964a9d9dcc39e5dfb3c25473a34
boaslehrke <boas.lehrke@live.de>
2025-04-03 11:35:07 +0200

    init project

---
+++ src/main/java/de/lehrke/agiledocsfrontend/agiledocsfrontend/AgiledocsfrontendApplication.java
@@ -0,0 +1,13 @@
+package de.lehrke.agiledocsfrontend.agiledocsfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiledocsfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiledocsfrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lehrke/agiledocsfrontend/agiledocsfrontend/AgiledocsfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lehrke.agiledocsfrontend.agiledocsfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiledocsfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
